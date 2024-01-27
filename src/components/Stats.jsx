69c8c08d3a81b65da1ffc089b9b8e910594770b0
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-28 01:19:18 +0530

    Implement packing statistics

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,22 @@
-export default function Stats() {
+export default function Stats({ items }) {
+	if (items.length === 0)
+		return (
+			<footer className="stats">
+				<em>Start adding items for your travel!</em>
+			</footer>
+		);
+
+	const numItems = items.length;
+	const numPackedItems = items.filter(item => item.packed).length;
+	const percentagePacked = Math.round((numPackedItems / numItems) * 100);
+
 	return (
-		<div className="stats">
-			<em>You have X items on your list, and you have already packed Y (Z%)</em>
-		</div>
+		<footer className="stats">
+			<em>
+				{percentagePacked === 100
+					? 'Your packing is complete! Enjoy your trip!'
+					: `You have ${numItems} items on your list, and you have already packed ${numPackedItems} items (${percentagePacked}%)`}
+			</em>
+		</footer>
 	);
 }
